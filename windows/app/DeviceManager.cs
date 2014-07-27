85b05f89e2ea5326c9ea92e265e6fd4428ed2c20
Dmitry Sutyagin <f3flight@gmail.com>
2014-07-27 23:26:40 +0400

    Slow progress towards installing device within app
    Device gets created, still need to do driver install. Prolly will have
    to embed drivers for all versions with proper bitness (will have 2 vers
    of app - x32 and x64), extract them to temp folder and point the
    pinvoked function to that folder (to .inf file there). Later will need
    to think about uninstall.

--- windows/app/DeviceManager.cs
+++ windows/app/DeviceManager.cs
@@ -10,8 +10,19 @@ namespace SPenClient
     static class DeviceManager
     {
         const int INVALID_HANDLE = -1;
-        const UInt16 DICD_GENERATE_ID = 1;
+        const uint DICD_GENERATE_ID   = 0x00000001;
+        const uint DIF_REMOVE         = 0x00000005;
+        const uint DIF_REGISTERDEVICE = 0x00000019;
+        const uint SPDRP_HARDWAREID   = 0x00000001;
+
+        // Guid should match "ClassGuid" from driver's inf file. Otherwise need to to use SetupDiGetINFClass function
         static Guid f3flightGuid = new Guid("a59a8c19-ab59-4161-8f58-09ecad135546");
+
+        // ClassName should match "Class" from driver's inf file. Otherwise need to to use SetupDiGetINFClass function
+        static string ClassName = "SPenVirtualDevice";
+
+        // hwid should match "root\..." elements from driver's inf file.
+        static string hwid = "root\\spenvhid";
 
         [DllImport("setupapi.dll", SetLastError = true)]
         static extern IntPtr SetupDiCreateDeviceInfoList(ref Guid ClassGuid, IntPtr hwndParent);
@@ -28,24 +39,58 @@ namespace SPenClient
             public IntPtr Reserved;
         }
 
-        [DllImport("setupapi.dll", SetLastError = true)]
+        [DllImport("setupapi.dll", SetLastError = true, CharSet = CharSet.Unicode)]
         static extern Boolean SetupDiCreateDeviceInfo(
             IntPtr DeviceInfoSet,
-            char[] ClassName,
+            string ClassName,
             ref Guid ClassGUID,
             IntPtr DeviceDescription,
             IntPtr hwndParent,
-            UInt16 CreationFlags,
-            IntPtr DeviceInfoData
+            uint CreationFlags,
+            ref SP_DEVINFO_DATA DeviceInfoData
         );
+
+        [DllImport("setupapi.dll", SetLastError = true)]
+        static extern Boolean SetupDiSetDeviceRegistryProperty(
+            IntPtr DeviceInfoSet,
+            ref SP_DEVINFO_DATA DeviceInfoData,
+            uint Property,
+            string PropertyBuffer,
+            ushort PropertyBufferSize
+        );
+
+        //[DllImport("setupapi.dll", SetLastError = true)]
+        //static extern Boolean SetupDiGetDeviceRegistryProperty(
+        //    IntPtr DeviceInfoSet,
+        //    ref SP_DEVINFO_DATA DeviceInfoData,
+        //    uint Property,
+        //    out uint PropertyRegDataType,
+        //    byte[] PropertyBuffer,
+        //    uint PropertyBufferSize,
+        //    out uint RequiredSize
+        //);
+
+        [DllImport("setupapi.dll", SetLastError = true)]
+        static extern Boolean SetupDiCallClassInstaller(uint InstallFunction, IntPtr DeviceInfoSet, ref SP_DEVINFO_DATA DeviceInfoData);
 
         public static bool installDevice()
         {
+            Boolean bSuccess = false;
             IntPtr DeviceInfoSet = SetupDiCreateDeviceInfoList(ref f3flightGuid, IntPtr.Zero);
             SP_DEVINFO_DATA DeviceInfoData = new SP_DEVINFO_DATA();
             DeviceInfoData.cbSize = (uint)Marshal.SizeOf(DeviceInfoData);
             //if (DeviceInfoSet != ) {
-            SetupDiDestroyDeviceInfoList(DeviceInfoSet);
+            bSuccess = SetupDiCreateDeviceInfo(DeviceInfoSet, ClassName, ref f3flightGuid, IntPtr.Zero, IntPtr.Zero, DICD_GENERATE_ID, ref DeviceInfoData);
+            bSuccess = SetupDiSetDeviceRegistryProperty(DeviceInfoSet, ref DeviceInfoData, SPDRP_HARDWAREID, hwid, (ushort)hwid.Length);
+            //uint reqSize = 0;
+            //uint propRegDataType = 0;
+            //uint bufferSize = 50;
+            //byte[] propBuffer = new byte[50];
+            //StringBuilder pBuf = new StringBuilder(50);
+            //bSuccess = SetupDiGetDeviceRegistryProperty(DeviceInfoSet, ref DeviceInfoData, SPDRP_HARDWAREID, out propRegDataType, propBuffer, bufferSize, out reqSize);
+            bSuccess = SetupDiCallClassInstaller(DIF_REGISTERDEVICE, DeviceInfoSet, ref DeviceInfoData);
+            //bSuccess = SetupDiCallClassInstaller(DIF_REMOVE, DeviceInfoSet, ref DeviceInfoData);
+            bSuccess = SetupDiDestroyDeviceInfoList(DeviceInfoSet);
             return true;
         }
 
