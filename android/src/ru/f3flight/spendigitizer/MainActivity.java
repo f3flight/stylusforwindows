05124054b46d6d0e2a996ecab6c512624f0c491c
Dmitry Sutyagin <f3flight@gmail.com>
2014-08-18 17:05:13 +0400

    Autohide app bar after 10 sec, better display size handling

--- android/src/ru/f3flight/spendigitizer/MainActivity.java
+++ android/src/ru/f3flight/spendigitizer/MainActivity.java
@@ -17,6 +17,8 @@ import java.io.*;
 import java.lang.reflect.*;
 import java.net.*;
 import java.nio.*;
+import android.view.View.*;
+import java.util.*;
 
 public class MainActivity extends Activity {
 	
@@ -64,17 +66,18 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
 		
 		getWindow().getDecorView().setSystemUiVisibility(
-			View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+			View.SYSTEM_UI_FLAG_IMMERSIVE
 			| View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-//			| View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-//			| View.SYSTEM_UI_FLAG_FULLSCREEN
-			| View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+			| View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+			| View.SYSTEM_UI_FLAG_FULLSCREEN
+    		| View.SYSTEM_UI_FLAG_LAYOUT_STABLE
 		);
         
 		mSCanvas = (SCanvasView) findViewById(R.id.canvas_view);
 		mSCanvas.setSCanvasInitializeListener(mSCanvasInitializeListener);
 		mSCanvas.setSPenHoverListener(mSPenHoverListener);
 		mSCanvas.setSPenTouchListener(mSPenTouchListener);
+		getWindow().getDecorView().setOnSystemUiVisibilityChangeListener(visChangeListener);
     }
     
     @Override
@@ -84,7 +87,7 @@ public class MainActivity extends Activity {
 		
 		Display display = getWindowManager().getDefaultDisplay();
 		Point point = new Point();
-		display.getSize(point);
+		display.getRealSize(point);
 		screenWidth = point.x;
 		screenHeight = point.y;
 		screenProportions = 1.0f*screenWidth/screenHeight;
@@ -354,5 +357,41 @@ public class MainActivity extends Activity {
 		    }
 			
 	}
+	
+	OnSystemUiVisibilityChangeListener visChangeListener = new OnSystemUiVisibilityChangeListener() {
+
+		@Override
+		public void onSystemUiVisibilityChange(int p1)
+		{
+			Toast.makeText(mContext, "visibility is "+p1, Toast.LENGTH_SHORT).show();
+			if (p1 == 0) {
+				Timer t = new Timer();
+				TimerTask ttask = new TimerTask() {
+
+					@Override
+					public void run()
+					{
+						runOnUiThread(new Runnable(){
+
+							@Override
+							public void run()
+							{
+								getWindow().getDecorView().setSystemUiVisibility(
+									View.SYSTEM_UI_FLAG_IMMERSIVE
+									| View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+			                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+			                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+									| View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+								);
+							}	
+						});
+					}
+
+				};
+				t.schedule(ttask, 10000);
+			}
+		}
+		
+	};
 	
 }
