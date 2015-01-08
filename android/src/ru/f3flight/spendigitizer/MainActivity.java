3c1c37f6e3501eca60823d03bd3f61c5323582c8
f3flight <f3flight@gmail.com>
2015-01-08 18:05:52 +0300

    partial implementation
    this commit breaks core functionality for now

--- android/src/ru/f3flight/spendigitizer/MainActivity.java
+++ android/src/ru/f3flight/spendigitizer/MainActivity.java
@@ -22,8 +22,15 @@ import java.util.*;
 
 public class MainActivity extends Activity {
 	
-	private SCanvasView mSCanvas;
-	private Context mContext = null;
+	boolean debug = false;
+	
+	LinearLayout vMotion, vMargin, vDebug;
+	TextView vEvent, vAction, vX, vY, vButtons, vMaxX, vMaxY, vEventCounter, vPressure;
+	SeekBar sb;
+	long counter = 0L;
+	
+	Context c;
+	
 	private DatagramSocket socket;
 	private byte[] dtab =new byte[21]; //size of data packet
 	private DatagramPacket pack;
@@ -55,16 +62,23 @@ public class MainActivity extends Activity {
 	
     @Override
     public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
 		spenReport = ByteBuffer.allocate(dtab.length);
 		spenReport.order(ByteOrder.LITTLE_ENDIAN);
 //    	requestWindowFeature(Window.FEATURE_NO_TITLE);
 		
         StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
         StrictMode.setThreadPolicy(policy); 
-    	super.onCreate(savedInstanceState);
 
-    	mContext = this;
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.main);
+        
+		getWindow().getDecorView().setOnSystemUiVisibilityChangeListener(visChangeListener);
+    }
+    
+    @Override
+    protected void onResume() {
+        super.onResume();
 		
 		getWindow().getDecorView().setSystemUiVisibility(
 			View.SYSTEM_UI_FLAG_IMMERSIVE
@@ -73,27 +87,107 @@ public class MainActivity extends Activity {
 			| View.SYSTEM_UI_FLAG_FULLSCREEN
     		| View.SYSTEM_UI_FLAG_LAYOUT_STABLE
 		);
-        
-		mSCanvas = (SCanvasView) findViewById(R.id.canvas_view);
-		mSCanvas.setSCanvasInitializeListener(mSCanvasInitializeListener);
-		mSCanvas.setSPenHoverListener(mSPenHoverListener);
-		mSCanvas.setSPenTouchListener(mSPenTouchListener);
-		getWindow().getDecorView().setOnSystemUiVisibilityChangeListener(visChangeListener);
-    }
-    
-    @Override
-    protected void onResume() {
-        super.onResume();
+		
+		c = this;
+		
         Init();
 		
-		Display display = getWindowManager().getDefaultDisplay();
-		Point point = new Point();
-		display.getRealSize(point);
-		screenWidth = point.x;
-		screenHeight = point.y;
-		screenProportions = 1.0f*screenWidth/screenHeight;
-		//Toast.makeText(mContext, "prop:"+screenProportions, Toast.LENGTH_SHORT).show();
+		if (debug)
+		{
+			vDebug = (LinearLayout)findViewById(R.id.mainLinearLayoutDebug);
+			vDebug.setVisibility(View.VISIBLE);
+		}
+		
+		vMotion = (LinearLayout)findViewById(R.id.mainLinearLayoutMotion);
+		vMargin = (LinearLayout)findViewById(R.id.mainLinearLayoutMargin);
+		
+		OnGenericMotionListener ogml = new OnGenericMotionListener()
+		{
+			@Override
+			public boolean onGenericMotion(View p1, MotionEvent p2)
+			{
+				logMotion("onGenericMotion", p2);
+				return true;
+			}	
+		};
+		v.setOnGenericMotionListener(ogml);
+		
+		OnTouchListener otl = new OnTouchListener()
+		{
+			@Override
+			public boolean onTouch(View p1, MotionEvent p2)
+			{
+				logMotion("onTouch", p2);
+				return true;
+			}
+		};
+		v.setOnTouchListener(otl);
+		
+		OnHoverListener ohl = new OnHoverListener()
+		{
+			@Override
+			public boolean onHover(View p1, MotionEvent p2)
+			{
+				logMotion("onHover", p2);
+				return true;
+			}
+		};
+		v.setOnHoverListener(ohl);
+		
+		vX = (TextView)findViewById(R.id.mainTextViewX);
+		vY = (TextView)findViewById(R.id.mainTextViewY);
+		vMaxX = (TextView)findViewById(R.id.mainTextViewMaxX);
+		vMaxY = (TextView)findViewById(R.id.mainTextViewMaxY);
+		vEventCounter = (TextView)findViewById(R.id.mainTextViewEventCounter);
+		vPressure = (TextView)findViewById(R.id.mainTextViewPressure);
+		vEvent = (TextView)findViewById(R.id.mainTextViewEvent);
+		vAction = (TextView)findViewById(R.id.mainTextViewAction);
+		vButtons = (TextView)findViewById(R.id.mainTextViewButtons);
+		
+		sb = (SeekBar)findViewById(R.id.mainSeekBar1);
+		OnSeekBarChangeListener osbcl = new OnSeekBarChangeListener()
+		{
+
+			@Override
+			public void onProgressChanged(SeekBar p1, int p2, boolean p3)
+			{
+				ViewGroup.MarginLayoutParams lp = (ViewGroup.MarginLayoutParams)vMargin.getLayoutParams();
+				lp.setMargins(p2,p2,p2,p2);
+				vMargin.setLayoutParams(lp);
+			}
+
+			@Override
+			public void onStartTrackingTouch(SeekBar p1)
+			{
+				// TODO: Implement this method
+			}
+
+			@Override
+			public void onStopTrackingTouch(SeekBar p1)
+			{
+				// TODO: Implement this method
+			}	
+		};
+		sb.setOnSeekBarChangeListener(osbcl);
+		screenProportions = 1.0f*vMargin.getWidth()/vMargin.getHeight();
     }
+	
+	void logMotion(String ListenerName, MotionEvent p2)
+	{
+		if (debug)
+		{
+			vEvent.setText(ListenerName);
+			vAction.setText(String.valueOf(p2.getActionMasked()));
+			vX.setText(String.valueOf(p2.getX()));
+			vY.setText(String.valueOf(p2.getY()));
+			vButtons.setText(String.valueOf(p2.getButtonState()));
+			vMaxX.setText(String.valueOf(vMargin.getWidth()));
+			vMaxY.setText(String.valueOf(vMargin.getHeight()));
+			vPressure.setText(String.valueOf(p2.getPressure()));
+			counter++;
+			vEventCounter.setText(String.valueOf(counter));
+		}	
+	}
 
 	private void setInputXY(float x, float y)
 	{
